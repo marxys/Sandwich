7c8f0542f257f0f9754838bdd8ee1d3aa20471e1
Florentin Rochet <oversnals@hotmail.com>
2012-04-01 19:07:26 +0200

    ajout controller + vue acceuil + modules

--- app_sandwitch_293/controllers/Pages.php
+++ app_sandwitch_293/controllers/Pages.php
@@ -1,13 +1,15 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Pages extends CI_Controller{
 
 	function __construct(){
 		parent::__construct();
-		
 	}
 	// Chargé par défaut.
 	public function index(){
+		$this->load->view('modules/header'); 
 		$this->load->view('acceuil');
+		$this->load->view('modules/footer'); 
+		//$this->load->view('welcome_message');
 	}
 	
 	public function contact(){
---
+++ app_sandwitch_293/views/acceuil.php
@@ -0,0 +1,7 @@
+
+
+<?php echo 'test'; 
+/*
+	Page d'acceuil : Contient le formulaire de login et l'acceuil par transparence en dessous.
+	
+*/
