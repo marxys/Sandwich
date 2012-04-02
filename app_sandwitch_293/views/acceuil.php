2cdf3f9338151ad2262d11c32bb7bcfa0f552c88
Florentin Rochet <oversnals@hotmail.com>
2012-04-02 15:47:21 +0200

    ajout librairie css

--- app_sandwitch_293/views/acceuil.php
+++ app_sandwitch_293/views/acceuil.php
@@ -3,9 +3,9 @@ $(document).ready(function(){
 	$("#form_login").submit(function(event){
 		event.preventDefault(); // Supprime l'action par defaut du formulaire submit (rechargement de page)
 		// Verification que les formulaires sont remplis
-		if(checkEmptyForm(new array('user','pwd'))){
+		if(checkEmptyForm(new Array('login','password'))){
 			$.ajax({
-				type: POST,
+				type: 'POST',
 				url :'../controller/ajax/ajax_request/',
 				data :'p=connexion&login='+$("#login").val()+'&password='+$("#password").val(),
 				dataType: 'json',
@@ -21,18 +21,22 @@ $(document).ready(function(){
 			updateTips('Veuillez remplir tous les éléments de formulaire requis.');
 		}
 	});
+	$(function(){
+		$("#button").button();
+	});
 });
+
 </script>
 
 <div id="conteneur_form">
 	<p class="updateTips"> Veuillez rentrez vos information de connexion. </p>
 	<form id="form_login" method="post">
 	<h5>Nom d'utilisateur</h5>
-	<input id="login" type="text" name="username" value="" size="50" />
+	<input id="login" type="text" name="username" value="" size="50" class="text ui-widget-content ui-corner-all" />
 
 	<h5>Mot de passe</h5>
-	<input id="password" type="password" name="password" value="" size="50" /> <br/>
-    <input type="submit" value="Connexion"/>
+	<input id="password" type="password" name="password" value="" size="50" class="text ui-widget-content ui-corner-all" /> <br/>
+    <input id="button" type="submit" value="Connexion"/>
     </form>
   
 </div>
